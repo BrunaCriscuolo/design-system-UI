2856c6a3ab91fb9779bb2d7e4f333b09f2ac36ac
BrunaCriscuolo <bruna.criscuolo@hotmail.com>
2021-07-15 10:46:59 -0300

    feat: add documentation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,4 +4,9 @@ type ButtonProps = {
   children: ReactNode;
 };
 
+/**
+- Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eum cupiditate neque sint accusamus amet nam. Sed eum dicta maxime.
+**/
+
+
 export const Button = ({ children }: ButtonProps): ReactElement => <button type="button">{children}</button>;
--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -5,7 +5,13 @@ import { Button } from './Button';
 export default {
   component: Button,
   title: 'Component/Button',
+
+  parameters: {
+    component: Button,
+    componentSubtitle: 'Para você clicar :)',
+  },
 } as Meta;
+
 
 const Template: Story = ({ label }) => <Button>{label}</Button>;
 
